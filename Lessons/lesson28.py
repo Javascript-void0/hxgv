dcede153b6eb9937514081ee9c607f2e1cd1a8ae
Java <indead1124@gmail.com>
2021-05-16 13:11:16 -0400

    inout

--- Lessons/lesson28.py
+++ Lessons/lesson28.py
@@ -2,7 +2,24 @@ fin = open('./Lessons/lesson28.in', 'r')
 line = fin.readline().split()
 print(line)
 
+def teleport(a, b, x, y):
+    dab = abs(b-a)
+    daxyb = abs(x-a) + abs(b-y)
+    dayxb = abs(y-a) + abs(b-x)
+    ans = dab
+    if daxyb < ans:
+        ans = daxyb
+    if dayxb < ans:
+        ans = dayxb
+    return ans
+
 a = int(line[0])
 b = int(line[1])
 x = int(line[2])
 y = int(line[3])
+
+res = teleport(a, b, x, y)
+fout = open('./Lessons/lesson28.out', 'w')
+fout.write(str(res))
+fout.close()
+fin.close()
